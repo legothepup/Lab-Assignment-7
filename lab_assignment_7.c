e0d1441646c9c7dea0a5902f0cce95f13678645f
Aidan Huggins <119627309+legothepup@users.noreply.github.com>
2024-03-07 23:35:08 -0500

    lab_assignment_7.c

--- lab_assignment_7.c
+++ lab_assignment_7.c
@@ -5,34 +5,30 @@
 
 #include <stdio.h>
 
-void bubbleSort(int arr[], int n, int swaps[]) 
-{
+void bubbleSort(int arr[], int n, int swaps[]) {
     int temp, i, j;
     for (i = 0; i < n-1; i++) {
         for (j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) 
-            {
+            if (arr[j] > arr[j+1]) {
                 // Swap elements
                 temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
 
                 // Increment swaps for the swapped elements
-                swaps[j]++;
-                swaps[j+1]++;
+                swaps[arr[j]]++;
+                swaps[arr[j+1]]++;
             }
         }
     }
 }
 
-void selectionSort(int arr[], int n, int swaps[]) 
-{
+void selectionSort(int arr[], int n, int swaps[]) {
     int i, j, minIndex, temp;
     for (i = 0; i < n-1; i++) {
         minIndex = i;
         for (j = i+1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) 
-            {
+            if (arr[j] < arr[minIndex]) {
                 minIndex = j;
             }
         }
@@ -42,27 +38,24 @@ void selectionSort(int arr[], int n, int swaps[])
         arr[i] = temp;
 
         // Increment swaps for the swapped elements
-        swaps[i]++;
-        swaps[minIndex]++;
+        swaps[arr[i]]++;
+        swaps[arr[minIndex]]++;
     }
 }
 
-//Prints arrays
-void printResults(int arr[], int n, int swaps[], char *sortName) 
-{
+void printResults(int arr[], int n, int swaps[], char *sortName) {
     printf("%s:\n", sortName);
     for (int i = 0; i < n; i++) {
-        printf("%d: %d swaps\n", arr[i], swaps[i]);
+        printf("%d: %d swaps\n", arr[i], swaps[arr[i]]);
     }
     int totalSwaps = 0;
     for (int i = 0; i < n; i++) {
-        totalSwaps += swaps[i];
+        totalSwaps += swaps[arr[i]];
     }
     printf("total # of swaps: %d\n\n", totalSwaps);
 }
 
-int main() 
-{
+int main() {
     int array1[] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
     int array2[] = {90, 80, 70, 60, 50, 40, 30, 20, 10};
     int n1 = sizeof(array1) / sizeof(array1[0]);
@@ -79,13 +72,16 @@ int main()
     bubbleSort(array2, n2, swaps2);
     printResults(array2, n2, swaps2, "Bubble Sort");
 
+    // Reset swaps arrays
+    int resetSwaps[100] = {0};
+
     // Perform Selection Sort on array1
-    selectionSort(array1, n1, swaps1);
-    printResults(array1, n1, swaps1, "Selection Sort");
+    selectionSort(array1, n1, resetSwaps);
+    printResults(array1, n1, resetSwaps, "Selection Sort");
 
     // Perform Selection Sort on array2
-    selectionSort(array2, n2, swaps2);
-    printResults(array2, n2, swaps2, "Selection Sort");
+    selectionSort(array2, n2, resetSwaps);
+    printResults(array2, n2, resetSwaps, "Selection Sort");
 
     return 0;
 }
